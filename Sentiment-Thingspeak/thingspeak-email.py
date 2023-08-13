4218d220ae5253420b5bce55801442ac246519ba
Diazonic Labs <61781809+diazoniclabs@users.noreply.github.com>
2023-08-13 18:08:56 +0530

    Update thingspeak-email.py

--- Sentiment-Thingspeak/thingspeak-email.py
+++ Sentiment-Thingspeak/thingspeak-email.py
@@ -3,7 +3,7 @@ import time
 import json
 
 while True:
-  url = "https://api.thingspeak.com/channels/1686611/feeds.json?results=1"
+  url = "https://api.thingspeak.com/channels/1686611/feeds.json?results=1" # Copy your Read API request and change results to 1
   response = requests.request("GET", url)
   op = response.json()['feeds'][0]['field1']
   op = int(op)
@@ -15,10 +15,12 @@ while True:
   "body": f"Above 40 degrees. Its {op}"})
 
   headers = {
-    'ThingSpeak-Alerts-API-Key': '',
+    'ThingSpeak-Alerts-API-Key': '', # Put your Alerts API key here
     'Content-Type': 'application/json'}
 
-  response = requests.request("POST", url, headers=headers, data=payload)
-
-  print(response.text)
+  if(op>40): 
+    # 40 is the threshold value
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
+    
   time.sleep(10)
